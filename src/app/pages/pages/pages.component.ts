7b7228265565e8b298ff7c2798993c91288edb7b
Maria Fernanda Ulate Fonseca <fernanda-45@hotmail.es>
2022-06-03 12:06:18 -0600

    section 3

---
+++ src/app/pages/pages/pages.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html'
+})
+export class PagesComponent implements OnInit {
+
+  year = new Date().getFullYear();
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
